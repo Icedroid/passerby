a75dcfd531431235ee4c4b40b4c9346e7a716a25
wiwen <shenggxhz@gmail.com>
2018-07-21 17:52:13 +0800

    fix bugs

--- api/modules/v1/actions/user/ProfileAction.php
+++ api/modules/v1/actions/user/ProfileAction.php
@@ -26,7 +26,6 @@ class ProfileAction extends Action
         if ($this->checkAccess) {
             call_user_func($this->checkAccess, $this->id, $model);
         }
-//        return $model->view_count;
 
         $fields = $model->fields();
         $fields['view_count'] = 'view_count'; //访客次数
@@ -34,9 +33,14 @@ class ProfileAction extends Action
         $fields['star'] = function ($model) {//好评值
             return intval($model->star * 100);
         };
-        $fields['money'] = function ($model){//金钱
-            //TO DO
+        $fields['money'] = function ($model){//赚钱=总收入
+            return $model->income/100;
+        };
+        $fields['balance'] = function ($model){//余额
             return $model->balance/100;
+        };
+        $fields['consume'] = function ($model){//总消费
+            return $model->consume/100;
         };
         $fields['collect_count'] = function ($model){//被收藏数
             return intval(UserCollect::find()->where(['uid'=>$model->id])->count('id'));
